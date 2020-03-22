3f24a49cf5ec704f13232de51d1cde30acead2f5
Mati <mwiczuk99@gmail.com>
2020-03-22 17:36:09 +0100

    Add ContactController

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ContactAPI.DataContext;
+using ContactAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : Controller
+    {
+        private readonly ContactContext _context;
+
+        public ContactController(ContactContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/contacts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contact>>> GetContacts()
+        {
+            return await _context.Contacts.ToListAsync();
+        }
+
+
+        // GET: api/contact/id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contact>> GetContact(int? id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return contact;
+        }
+
+
+        // POST: api/contact/id
+        [HttpPost]
+        public async Task<ActionResult<Contact>> PostContact(Contact contact)
+        {
+            _context.Contacts.Add(contact);
+            await _context.SaveChangesAsync();
+
+            return await GetContact(contact.ID);
+        }
+    }
+}
