eeed739c6741a21612c7f91d2a5c96ea9737b2ab
Mati <mwiczuk99@gmail.com>
2020-03-22 16:28:05 +0100

    Add ContactContext class

---
+++ DataContext/ContactContext.cs
@@ -0,0 +1,15 @@
+﻿using ContactAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactAPI.DataContext
+{
+    public class ContactContext : DbContext
+    {
+
+        public ContactContext(DbContextOptions<ContactContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
