8bc0ef01ac249443b49da7b68f6abd7268f2f3f6
Mati <mwiczuk99@gmail.com>
2020-03-22 15:34:25 +0100

    Add Contact model

---
+++ Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using ContactAPI.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactAPI.Models
+{
+    public class Contact : IContact
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(400, MinimumLength = 5, ErrorMessage = "Message length not between [5, 400]")]
+        public string Message { get; set; }
+    }
+}
---
+++ Models/Interfaces/IContact.cs
@@ -0,0 +1,9 @@
+﻿namespace ContactAPI.Models.Interfaces
+{
+    public interface IContact
+    {
+        int ID { get; set; }
+        string Email { get; set; }
+        string Message { get; set; }
+    }
+}
