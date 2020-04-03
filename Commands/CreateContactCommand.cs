b7f4510419d34460a90c1f9d27c245d3e96d6dcf
Mati <mwiczuk99@gmail.com>
2020-04-03 20:43:07 +0200

    Add commands for Contact

---
+++ Commands/CreateContactCommand.cs
@@ -0,0 +1,11 @@
+﻿using ContactAPI.Domain.Response;
+using ContactAPI.Models;
+using MediatR;
+
+namespace ContactAPI.Commands
+{
+    public class CreateContactCommand : IRequest<ContactResponse>
+    {
+        public Contact Contact { get; set; }
+    }
+}
