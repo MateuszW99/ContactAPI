65b7d1b007d6e93d09a06d7ae6fec96dc47ca50e
Mati <mwiczuk99@gmail.com>
2020-04-03 20:43:22 +0200

    Add queries for Contact

---
+++ Queries/GetAllContactsQuery.cs
@@ -0,0 +1,14 @@
+﻿using ContactAPI.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactAPI.Queries
+{
+    public class GetAllContactsQuery : IRequest<IEnumerable<Contact>>
+    {
+
+    }
+}
---
+++ Queries/GetContactByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using ContactAPI.Domain.Response;
+using MediatR;
+
+namespace ContactAPI.Queries
+{
+    public class GetContactByIdQuery : IRequest<ContactResponse>
+    {
+        public int Id { get; set; }
+
+        public GetContactByIdQuery(int id)
+        {
+            Id = id;
+        }
+
+    }
+}
